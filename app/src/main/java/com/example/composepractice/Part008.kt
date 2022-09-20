1868663be1f753723e60fb88c9788a43713468bb
adicomdotir <adicom.ir@gmail.com>
2022-09-20 12:36:53 +0430

    Sample lists

---
+++ app/src/main/java/com/example/composepractice/Part008.kt
@@ -0,0 +1,29 @@
+package com.example.composepractice
+
+import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.layout.padding
+import androidx.compose.foundation.lazy.LazyColumn
+import androidx.compose.material.Text
+import androidx.compose.runtime.Composable
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.text.font.FontWeight
+import androidx.compose.ui.text.style.TextAlign
+import androidx.compose.ui.unit.dp
+import androidx.compose.ui.unit.sp
+
+@Composable
+fun MainPart008() {
+    LazyColumn {
+        items(1000) {
+            Text(
+                text = "Item $it",
+                fontSize = 24.sp,
+                fontWeight = FontWeight.Bold,
+                textAlign = TextAlign.Center,
+                modifier = Modifier
+                    .fillMaxSize()
+                    .padding(vertical = 24.dp)
+            )
+        }
+    }
+}
